693d2cad14bfd786263316e795ab3bb8c206620c
Guilherme Bessa <128107879+Guiezz@users.noreply.github.com>
2025-08-23 13:46:19 -0300

    feat: adicionar configuração ESLint e variável de ambiente para a URL da API

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const config: NextConfig = {
+  // Adicione esta secção eslint
+  eslint: {
+    // Atenção: Isto permite que builds de produção sejam concluídos
+    // mesmo que o seu projeto tenha erros de ESLint.
+    ignoreDuringBuilds: true,
+  },
+  
+  // A sua configuração de imagens existente permanece aqui
   images: {
     remotePatterns: [
       {
