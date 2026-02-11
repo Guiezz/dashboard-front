916d9be8790b25ef29b2fa53dcf67feb58d3369b
Guilherme Bessa <guibessa@alu.ufc.br>
2026-02-11 13:01:36 -0300

    ajuste na cor da sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -94,7 +94,6 @@ export function Sidebar({ isCollapsed }: SidebarProps) {
                 </Link>
               </TooltipTrigger>
               <TooltipContent side="right">
-                {" "}
                 Implementação dos Planos de Secas{" "}
               </TooltipContent>
             </Tooltip>
@@ -159,7 +158,7 @@ export function Sidebar({ isCollapsed }: SidebarProps) {
               <TooltipTrigger asChild>
                 <Link
                   href="/simulacao"
-                  className={`flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary ${isActive("/responsaveis") ? "bg-muted text-primary" : "text-muted-foreground"} ${
+                  className={`flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary ${isActive("/simulacao") ? "bg-muted text-primary" : "text-muted-foreground"} ${
                     isCollapsed ? "justify-center" : "gap-3"
                   }`}
                 >
